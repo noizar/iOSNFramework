edacb3df5e96a72ecbc39562ae768f65ca85c688
Noizar <noizar.jr@gmail.com>
2019-01-30 14:38:58 +0700

    Initial commit

---
+++ Example/iOSNFramework/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iOSNFramework
+//
+//  Created by Noizar on 01/30/2019.
+//  Copyright (c) 2019 Noizar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
